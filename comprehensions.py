60a9c99af96043913b09ff544ba09a5c13cf65b3
Andreas Achrainer <161012467+achrainer1112@users.noreply.github.com>
2025-10-15 15:46:06 +0200

    Update comprehensions.py

--- comprehensions.py
+++ comprehensions.py
@@ -7,9 +7,12 @@ def set_comprehension(wort):
     return vokale
 
 def dict_comprehension(woerter):
-
     laenge = {wort: len(wort) for wort in woerter}
     return laenge
+
+def abc_comprehension():
+    alphabet = {letter: i for i, letter in enumerate(string.ascii_lowercase, start=1)}
+    return alphabet;
 
 
 def main():
@@ -20,6 +23,7 @@ def main():
     print(list_comprehension(r))
     print(set_comprehension(wort))
     print(dict_comprehension(woerter))
+    print(abc_comprehension())
 
 if __name__ == '__main__':
     main()
