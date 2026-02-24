6b08f3179ddc0e2ea3b6696bec9c53ee2bf9ef14
achrainer1112 <achrainera@gmail.com>
2026-02-24 16:29:29 +0100

    TimerDecorator added

---
+++ Poker/Poker.py
@@ -0,0 +1,160 @@
+import random
+import unittest
+from TimerDecorator import timer
+
+def deckErzeugen():
+    # Farbe: 0 = Herz  |  1 = Karo  |  2 = Pik  |  3 = Kreuz
+    # Wert: 2-10 = Zahlenkarten  |  11 = Bube (J)  |  12 = Dame (Q)  |  13 = König (K)  |  14 = Ass (A)
+    deck = [(farbe, wert) for farbe in range(4) for wert in range(2, 15)]
+    return deck
+
+
+def ziehung(deck, anzahlKarten):
+    return random.sample(deck, anzahlKarten)
+
+
+# Kombinationen
+def hatPaar(gezogeneKarten):
+    haeufigkeiten = zaehleHaeufigkeiten(gezogeneKarten)
+    return 2 in haeufigkeiten.values()
+
+
+def hatZweiPaare(gezogeneKarten):
+    haeufigkeiten = zaehleHaeufigkeiten(gezogeneKarten)
+    return list(haeufigkeiten.values()).count(2) == 2
+
+
+def hatDrilling(gezogeneKarten):
+    haeufigkeiten = zaehleHaeufigkeiten(gezogeneKarten)
+    return 3 in haeufigkeiten.values()
+
+
+def hatStrasse(gezogeneKarten):
+    werte = werteSortieren(gezogeneKarten)
+
+    # Normaler Fall: aufeinanderfolgende Werte prüfen
+    ist_strasse = True
+    for i in range(1, len(werte)):
+        if (werte[i] - 1) != werte[i - 1]:
+            ist_strasse = False
+            break
+
+    if ist_strasse:
+        return True
+
+    # Sonderfall: Ass als niedrigste Karte (A-2-3-4-5)
+    if 14 in werte:
+        if werte == [2, 3, 4, 5, 14]:
+            return True
+
+    return False
+
+
+def hatFlush(gezogeneKarten):
+    farben = farbenExtrahieren(gezogeneKarten)
+    for i in range(1, len(farben)):
+        if farben[i] != farben[i - 1]:
+            return False
+    return True
+
+
+def hatFullHouse(gezogeneKarten):
+    return hatPaar(gezogeneKarten) and hatDrilling(gezogeneKarten)
+
+
+def hatVierling(gezogeneKarten):
+    haeufigkeiten = zaehleHaeufigkeiten(gezogeneKarten)
+    return 4 in haeufigkeiten.values()
+
+
+def hatStraigthFlush(gezogeneKarten):
+    return hatFlush(gezogeneKarten) and hatStrasse(gezogeneKarten)
+
+
+def hatRoyalFlush(gezogeneKarten):
+    return hatStraigthFlush(gezogeneKarten) and werteSortieren(gezogeneKarten)[0] == 10
+
+
+# Hilfsfunktionen
+
+def farbenExtrahieren(gezogeneKarten):
+    return [karte[0] for karte in gezogeneKarten]
+
+
+def werteExtrahieren(gezogeneKarten):
+    return [karte[1] for karte in gezogeneKarten]
+
+
+def werteSortieren(gezogeneKarten):
+    werte = werteExtrahieren(gezogeneKarten)
+    werte.sort()
+    return werte
+
+
+def zaehleHaeufigkeiten(gezogeneKarten):
+    haeufigkeiten = {}
+    werte = werteExtrahieren(gezogeneKarten)
+    for wert in werte:
+        if (wert in haeufigkeiten):
+            haeufigkeiten[wert] += 1
+        else:
+            haeufigkeiten[wert] = 1
+    return haeufigkeiten
+
+
+def auswertung(gezogeneKarten, kombinationen):
+    if (hatRoyalFlush(gezogeneKarten)):
+        kombinationen["Royal Flush"] += 1
+    elif hatStraigthFlush(gezogeneKarten):
+        kombinationen["Straight Flush"] += 1
+    elif hatVierling(gezogeneKarten):
+        kombinationen["Vierling"] += 1
+    elif hatFullHouse(gezogeneKarten):
+        kombinationen["Full House"] += 1
+    elif hatFlush(gezogeneKarten):
+        kombinationen["Flush"] += 1
+    elif hatStrasse(gezogeneKarten):
+        kombinationen["Strasse"] += 1
+    elif hatDrilling(gezogeneKarten):
+        kombinationen["Drilling"] += 1
+    elif hatZweiPaare(gezogeneKarten):
+        kombinationen["Zwei Paare"] += 1
+    elif hatPaar(gezogeneKarten):
+        kombinationen["Paar"] += 1
+
+@timer
+def analyse(kombinationen, durchlaeufe):
+    print("Wahrscheinlichkeiten:")
+    for karte in kombinationen:
+        wert = kombinationen[karte]
+        print(karte + ": " + str(round((wert / durchlaeufe) * 100, 3)) + "%")
+
+
+def main():
+    kombinationen = {
+        "Royal Flush": 0,
+        "Straight Flush": 0,
+        "Vierling": 0,
+        "Full House": 0,
+        "Flush": 0,
+        "Strasse": 0,
+        "Drilling": 0,
+        "Zwei Paare": 0,
+        "Paar": 0,
+    }
+
+
+    durchlaeufe = 1000000
+    deck = deckErzeugen()
+
+    for i in range(durchlaeufe):
+        gezogeneKarten = ziehung(deck, 5)
+        auswertung(gezogeneKarten, kombinationen)
+
+    print(kombinationen)
+    analyse(kombinationen, durchlaeufe)
+
+
+if __name__ == "__main__":
+    main()
+    # unittest.main()
